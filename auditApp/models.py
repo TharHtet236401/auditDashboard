ccb8970cea2eb2aab9b587f1ed9443bf75038fb3
TharHtet236401 <tharhteta3@gmail.com>
2025-03-12 00:28:26 +0700

    Add Transaction model and register in admin panel
    - Created Transaction model with fields for amount, status, timestamp, merchant, and approval
    - Registered Transaction model in Django admin interface
    - Added model validation and choices for transaction status

--- auditApp/models.py
+++ auditApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+class Transaction(models.Model):
+    status = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+    ]
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)],null=False, blank=False)
+    status = models.CharField(max_length=255, choices=status, null=False, blank=False,default='pending')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    merchant =models.CharField(max_length=255, null=False, blank=False)
+    isFlagged = models.BooleanField(default=False,null=True, blank=True)
+    approved_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+
+    def __str__(self):
+        return f"{self.merchant} - {self.amount} - {self.status}"
