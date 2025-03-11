e9f41485be0213a8580818aaadfba897fc8e9f6e
TharHtet236401 <tharhteta3@gmail.com>
2025-03-12 02:00:57 +0700

    Revert "Display transactions on home page"
    This reverts commit 05231a4efee0f602be33153e2190ef76e2a249b4.

--- auditApp/views.py
+++ auditApp/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from .models import Transaction
 
 # Create your views here.
 def home(request):
-    transactions = Transaction.objects.all()
-    return render(request, 'home.html', {'transactions': transactions})
+    return render(request, 'home.html')
 
