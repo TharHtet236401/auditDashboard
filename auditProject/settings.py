083e033de24fdfbd2dec257415e12cb80e4c69f6
TharHtet236401 <tharhteta3@gmail.com>
2025-03-25 16:28:04 +0700

    Updated all_history.html to display timestamps in local time and modified settings.py to change the time zone to Asia/Bangkok, ensuring accurate time representation in the application.

--- auditProject/settings.py
+++ auditProject/settings.py
@@ -111,11 +111,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+USE_TZ = True
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 
-USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
